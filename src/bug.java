a2405320063478e1154950bd34ea40a485f69889
HXM-L <2602137886@qq.com>
2021-04-01 20:16:55 +0800

    方法

--- src/bug.java
+++ src/bug.java
@@ -7,5 +7,7 @@ public class bug {
 	 * 4.el表达是要会
 	 	* 需要注意的是：EL表达式点运算符（.）后面跟的属性名不是类的字段，而是GET/SET访问器去掉前缀的名字。
 		*比如：getName(),取属性时应当使用name,且第一个字母需要小写。
+	*5.点击commit and Push提示不能连接到github时,有可能是访问不了github,可以在cmd窗口ping一下
+	*输入:ping github.com
 	 * */
 }
